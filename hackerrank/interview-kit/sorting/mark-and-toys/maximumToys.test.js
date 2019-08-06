4a129cb3f3c1b66512aed79c1dbbf5a4a28585c2
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-06 16:37:08 +0300

    c

--- hackerrank/interview-kit/sorting/mark-and-toys/maximumToys.test.js
+++ hackerrank/interview-kit/sorting/mark-and-toys/maximumToys.test.js
@@ -1,32 +1,13 @@
 import {maximumToys} from './maximumToys';
 
-<<<<<<< HEAD
-describe('maximumToys', () => {
-
-    it('simple test', () => {
-        expect(maximumToys([1, 2, 3, 4], 7))
-            .toEqual(3);
-    });
-
-    it('bigger one', () => {
-=======
 describe('countSwaps', () => {
     it('first', () => {
->>>>>>> upstream/master
         expect(maximumToys([1, 12, 5, 111, 200, 1000, 10], 50))
             .toEqual(4);
     });
 
-<<<<<<< HEAD
-    it('buy all', () => {
-        expect(maximumToys([10, 20, 30], 100000))
-            .toEqual(3);
-    });
-
-=======
     it('second', () => {
         expect(maximumToys([3, 7, 2, 9, 4], 15))
             .toEqual(3);
     });
->>>>>>> upstream/master
 });
