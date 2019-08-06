0753f09108d13d7400ebdaf92ae747c038ff3a94
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-06 17:41:44 +0300

    comment

--- data-structures/arrays/base-operations/intersection.js
+++ data-structures/arrays/base-operations/intersection.js
@@ -9,23 +9,18 @@
  */
 export const intersection = (...arrays) => {
     /*var arrays = arr1.concat(arr2,arr3,arr4);*/
-    var sameValues = [];
     console.log(arrays[0]);
     console.log(arrays[1]);
     if((arrays[0] == null) || (arrays[1] == null)) {
         return [];
     }
-    arrays[0].filter(function (element) {
+    var sameValues =  arrays[0].filter(function (element) {
         for (var i = 1; i < arrays.length; i++) {
-            if (arrays[i].indexOf(element) >= 0) {
-            
-            } else {
+            if (arrays[i].indexOf(element) === -1) {
                 return false;
             }
         }
-        if (true) {
-            sameValues.push(element);
-        }
+        return true;
     });
     console.log(sameValues);
     return sameValues;
