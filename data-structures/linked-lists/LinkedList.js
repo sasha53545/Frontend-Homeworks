4bb2aa20b3b873b8f45c76f4849b192304641364
obabichev <babichev.oleg.n@gmail.com>
2019-08-07 19:45:19 +0200

    Linked List insert

--- data-structures/linked-lists/LinkedList.js
+++ data-structures/linked-lists/LinkedList.js
@@ -11,8 +11,34 @@ export class LinkedList {
     }
 
     length() {
+
     }
 
     push(data) {
+
+    }
+
+    insert(index, data) {
+
+    }
+
+    /**
+     * This method is used for tests, but you can use it for your testing
+     *
+     * This method returns list as string like '4 -> 3 -> 2 -> 1 -> 0', Where 4 is the data of the root element
+     *
+     * @return {string}
+     */
+    toString() {
+        const data = [];
+        let node = this.root;
+        while (node && data.indexOf(node) === -1) {
+            data.push(node);
+            node = node.next;
+        }
+        return data
+            .map(node => node.data)
+            .join(' -> ');
     }
 }
+
