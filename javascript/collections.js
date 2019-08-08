c6f83def39c59842fcccd4275c4bd7dd1c28356b
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-08 18:05:14 +0300

    com

--- javascript/collections.js
+++ javascript/collections.js
@@ -4,6 +4,12 @@
  * @param callback (item, index, array) => newItem
  */
 export const map = (arr, callback) => {
+    var newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        newArray.push(callback(arr[i], i));
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 /**
@@ -12,6 +18,14 @@ export const map = (arr, callback) => {
  * @param callback (item, index, array) => boolean
  */
 export const filter = (arr, callback) => {
+    var newArray = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (callback(arr[i]) == true) {
+            newArray.push(arr[i]);
+        }
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 /**
@@ -22,4 +36,10 @@ export const filter = (arr, callback) => {
  * @return {*}
  */
 export const reduce = (arr, callback, defaultValue) => {
+    var sum = defaultValue;
+    for (var i = 0; i < arr.length; i++) {
+        sum = callback(sum ,arr[i], i);
+    }
+    console.log(sum);
+    return sum;
 };
