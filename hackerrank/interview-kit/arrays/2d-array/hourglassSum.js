c0d93d2c297c0807457bbca6808289f669bb07a6
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-08 18:00:27 +0300

    com

--- hackerrank/interview-kit/arrays/2d-array/hourglassSum.js
+++ hackerrank/interview-kit/arrays/2d-array/hourglassSum.js
@@ -4,5 +4,25 @@
  * @param arr
  */
 export function hourglassSum(arr) {
-
+    var maxRow = 3;
+    var maxColumn = 3;
+    var result = [];
+    for (var i = 0; i <= maxColumn; i++) {
+       for (var j = 0; j <= maxRow; j++) {
+           result.push(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]);
+       }
+    }
+    result.sort((a, b) => b - a);
+    console.log(result[0]);
+    return result[0];
 }
+
+/* j 0  1  2  3  4  5
+
+i 0 [1, 1, 1, 0, 0, 0],
+  1 [0, 1, 0, 0, 0, 0],
+  2 [1, 1, 1, 0, 0, 0],
+  3 [0, 0, 2, 4, 4, 0],
+  4 [0, 0, 0, 2, 0, 0],
+  5 [0, 0, 1, 2, 4, 0]
+*/
