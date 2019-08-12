888d06533570e60b80547f0826da5ed8980aa621
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-12 03:02:01 +0300

    comment

--- hackerrank/interview-kit/arrays/left-rotation/rotLeft.js
+++ hackerrank/interview-kit/arrays/left-rotation/rotLeft.js
@@ -6,4 +6,13 @@
  * @return {*}
  */
 export function rotLeft(a, d) {
+    if (d === 0) {
+        return a;
+    }
+
+    let left = a.slice(0, 1);
+    let right = a.slice(1);
+
+    console.log(a);
+    return rotLeft([...right, ...left], d-1);
 }
--- hackerrank/interview-kit/string-manipulations/makeAnagram/makeAnagram.js
+++ hackerrank/interview-kit/string-manipulations/makeAnagram/makeAnagram.js
@@ -6,5 +6,64 @@
  * @return {number}
  */
 export function makeAnagram(a, b) {
+    a = a.split('');
+    console.log(a);
 
+    b = b.split('');
+    console.log(b);
+
+   let array1 = [];
+   let array2 = [];
+   let obj1 = {};
+   let obj2 = {};
+   let current = 0;
+
+   a.forEach(element => {
+      if (b.indexOf(element) >= 0) {
+         return 0;
+      } else {
+         current++;
+      }   
+   });
+
+
+   b.forEach(element => {
+      if (a.indexOf(element) >= 0) {
+         return 0;
+      } else {
+         current++;
+      }   
+   });
+   a.forEach(element => {
+      if (!obj1[element]) {
+         obj1[element] = 1;
+      } else {
+         obj1[element]++;
+      }
+   });
+   console.log(obj1);
+
+   b.forEach(element => {
+      if (!obj2[element]) {
+         obj2[element] = 1;
+      } else {
+         obj2[element]++;
+      }
+   });
+   console.log(obj2);
+
+   for (let key in obj1) {
+      let sum = 0;
+      if (obj1[key] < obj2[key]) {
+         sum = obj2[key] - obj1[key];
+         current = current + sum;
+      } else if (obj1[key] > obj2[key]) {
+         sum = obj1[key] - obj2[key];
+         current = current + sum;
+      }
+   }
+
+
+   console.log(current);
+   return current;
 }
