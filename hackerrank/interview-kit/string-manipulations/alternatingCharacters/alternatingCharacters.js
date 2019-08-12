db9f5ff2891c8dd0259b605ddfb8edcfcebc02fd
Александр Казаков <aleksander@MacBook-Pro-Aleksandr.local>
2019-08-12 04:01:05 +0300

    c

--- hackerrank/interview-kit/string-manipulations/alternatingCharacters/alternatingCharacters.js
+++ hackerrank/interview-kit/string-manipulations/alternatingCharacters/alternatingCharacters.js
@@ -10,9 +10,22 @@ export function alternatingCharacters(s) {
     for (let i = 0; i < s.length; i++) {
         if (s[i] === s[i+1]) {
             sumLetter++;
-        } else if (s[i] === s[i+1] && sumLetter%2){
-            sumLetter--;
+            console.log(s);
         }
     }
+    console.log(sumLetter);
     return sumLetter;
 }
+//0 1 2 3 4 5 6 7 8 9
+//A A A B B B A A B B
+//
+//for i=0
+//1. s[0] === s[1] sum=1
+//2. s[1] === s[2] sum=2
+//3. s[2] === s[3] sum=2
+//4. s[3] === s[4] sum=1
+//5. s[4] === s[5] sum=2
+//6. s[5] === s[6] sum=2
+//7. s[6] === s[7] sum=1
+//8. s[7] === s[8] sum=1
+//9. s[8] === s[9] sum=2
